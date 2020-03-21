0d0b23768dd134aecc10f35cfdf61dc280f19b16
Hemanthsai <hemanth.saai@gmail.com>
2020-03-21 17:50:13 +0530

    Change: Function to find if line in c file is a function call Added:  Test.c file

--- Test.py
+++ Test.py
@@ -1,20 +1,39 @@
-data_ = "variable1=variable1*variable2/data_12_val+!(variable_3)-variable4<<variable5>>variable6^variable7>>(variable&&variable9);"
-opers = ['=', '+', '-', '*', '/', '>', '<', '!', '^', '&', '&&', '|', '||', '(', ')', ';']
-list_ = []
+import re
+
+# Contains C Identifiers
+c_Identifiers = ['=', '+', '-', '*', '/', '>', '<', '!', '^', '&', '&&', '|', '||', '(', ')', ';']
 
 
 # Input:   String
 # Output:  List
-# Function returns the list of Variables in a given string
-def list_variable_in_line(data):
-    for i in opers:
-        data = data.replace(i, ' ')
-    list_loc = data.split(' ')
-    list_loc = list(dict.fromkeys(list_loc))
-    list_loc.remove('')
-    return list_loc
+# Returns  True if the parameter is a function call
+def _is_a_function(_data):
+    _regex = r"\w\w*\(.*\);"
+    _check = re.search(_regex, _data)
+    if _check:
+        return True
+    else:
+        return False
 
 
-data2 = 'a = b + c'
-list_ = list_variable_in_line(data2)
-print(list_)
+# Input:   String
+# Output:  List
+# Returns the list of Variables in a given string
+def _list_variable_in_line(_data):
+    for _element in c_Identifiers:
+        _data = _data.replace(_element, ' ')
+    _list_loc = _data.split(' ')
+    _list_loc = list(dict.fromkeys(_list_loc))
+    while '' in _list_loc:
+        _list_loc.remove('')
+    return _list_loc
+
+
+file = open("Test.c", 'r')
+contents = file.readlines()
+
+for line in contents:
+    if _is_a_function(line):
+        print(line)
+    else:
+        print(_list_variable_in_line(line))
