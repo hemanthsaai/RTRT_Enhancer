c7e863e712e83d06a0e323c5a0404cf25a21ca6a
Hemanthsai <hemanth.saai@gmail.com>
2020-03-21 22:43:15 +0530

    Change: Function to remove comments Added:  ToDo.txt

--- Test.c
+++ Test.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>//Hey
 int main() {
     double num;
     int varr2;
     int var3 = 0;
     num = 20;
-    varr2 = var3;
-
+    varr2 = var3;//check if this comment is removed
+// check if this comment is removed
+        // check if this line is removed
     printf("Enter a number: ");
-    scanf("%lf", &num);
+    scanf("%lf", &num);  /*Is this Line Removed? */
+/* How about a */
+  /*  Multi Line
+    comment */
     if (num <= 0.0) {
         if ((num == 0.0) && (num == 0.2))
             printf("You entered 0.");
--- Test.py
+++ Test.py
@@ -4,11 +4,45 @@ import re
 c_Identifiers = ['=', '+', '-', '*', '/', '>', '<', '!', '^', '&', '&&', '|', '||', '(', ')', ';']
 
 
+# Input:   FileName including its Directory
+# Output:  String holding a full file without comments
+# Reads the file and removes comments completely
+def _remove_comments(_file):
+    _regex_block_comment_single_line = "/\*.*[\S]*\*/"
+    _regex_block_comment_multi_line = "/\*.*[\S\s]*\*/"
+    _regex_single_line = "//.*"
+    _file_descriptor = open(_file, 'r')
+    _contents = _file_descriptor.read()
+    _re_object = re.compile(_regex_block_comment_single_line)
+    _contents = _re_object.sub('', _contents)
+    _re_object = re.compile(_regex_block_comment_multi_line)
+    _contents = _re_object.sub('', _contents)
+    _re_object = re.compile(_regex_single_line)
+    _contents = _re_object.sub('', _contents)
+    _file_descriptor.close()
+
+    return _contents
+
+
 # Input:   String
 # Output:  List
 # Returns  True if the parameter is a function call
 def _is_a_function(_data):
     _regex = r"\w\w*\(.*\);"
+    _check = re.search(_regex, _data)
+    if _check:
+        return True
+    else:
+        return False
+
+
+# Input:   String
+# Output:  List
+# Returns  True if the parameter is an assignment
+# TODO Multi line Assignment
+#      statements to be considered
+def _is_an_assignment(_data):
+    _regex = r"\w.*=.*;"
     _check = re.search(_regex, _data)
     if _check:
         return True
@@ -59,13 +93,26 @@ def _list_variables_in_line(_data):
     return _list_loc
 
 
-file = open("Test.c", 'r')
-contents = file.readlines()
+def _functionality_test_1():
+    file = open("Test.c", 'r')
+    contents = file.readlines()
 
-for line in contents:
-    if _is_a_function(line):
-        print(line[:-1])
-    elif _is_a_condition(line):
-        print(line[:-1])
-    elif _is_a_loop(line):
-        print(line[:-1])
+    for line in contents:
+        if _is_a_function(line):
+            print(line[:-1])
+        elif _is_a_condition(line):
+            print(line[:-1])
+        elif _is_a_loop(line):
+            print(line[:-1])
+        elif _is_an_assignment(line):
+            print(line[:-1])
+
+    file.close()
+
+
+def _functionality_test_2():
+    data = _remove_comments("Test.c")
+    print(data)
+
+
+_functionality_test_2()
