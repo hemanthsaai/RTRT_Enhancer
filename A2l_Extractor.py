d51302030f43e80857a9e53b3b99fc009b358815
Hemanthsai <hemanth.saai@gmail.com>
2020-03-28 22:38:33 +0530

    Change: Resolved issue - A2l_extractor regular expression did                          not include variable name in it. So always                          a2l values are same
                             Fixed

--- A2l_Extractor.py
+++ A2l_Extractor.py
@@ -4,7 +4,7 @@ import re
 # Input:   Entire A2l File as a String
 # Output:  List with all available detail of A2l variable
 def _Capture_a2l_variable(_file_contents, _variable):
-    _regex = r"\/\w*\s\w*\s(.*)\s\"(.*)\"\n\s*(\w*)\s(\w*)\s(\d\s\d*\s)(.*)"
+    _regex = r"\/\w*\s\w*\s(" + _variable + r")\s\"(.*)\"\n\s*(\w*)\s(\w*)\s(\d\s\d*\s)(.*)"
     _extracted = re.search(_regex, _file_contents)
     if _extracted:
         return _extracted.groups()
